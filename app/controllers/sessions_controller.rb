cc4cd19fbaaf72209539ae0f1600309b953721a9
Chris Tonog <chris.tonog@gmail.com>
2012-04-03 20:00:47 -0400

    finished chapter 8.1, testing for invalid session requests

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email])
+
+		if user && user.authenticate(params[:session][:password])
+			# sign user in and redirect to the user's show page
+		else
+			flash.now[:error] = "Invalid email/password combination" # not quite right
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
+
+end
