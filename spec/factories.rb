dc84ad5d860ec122fbf56aa9aa1cfd173a2c46fd
Chris Tonog <chris.tonog@gmail.com>
2012-04-05 08:18:25 -0400

    finished through chapter 9.3

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 	factory :user do 
-		name "Chris Topher"
-		email "chris@test.org"
+		sequence(:name) { |n| "Person #{n}"}
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
 	end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,32 @@ require 'spec_helper'
 describe "User Pages" do
 	subject { page }
 
+	describe "index" do
+		let(:user) { FactoryGirl.create(:user) }
+
+		before do
+			sign_in user
+			visit users_path
+		end
+
+		it { should have_selector('title', text: 'All users') }
+
+		describe "pagination" do
+			before (:all) { 30.times { FactoryGirl.create(:user) } }
+			after(:all) { User.delete_all }
+
+			it { should have_link('Next') }
+			its(:html) { should match('>2</a>') }
+
+			it "should list each user" do
+				User.all[0..2].each do |user|
+					page.should have_selector('li', text: user.name)
+				end
+			end
+		end
+	end
+
+
 	describe "Signup Page" do
 		before { visit signup_path }
 
